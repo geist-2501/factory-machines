50dc038ddf01030ef55ee5ec7bce7a21eec83fa0
geist_2501 <blaironbute@gmail.com>
2023-01-21 11:50:19 +0000

    Initial impl of the environment

--- factory-machines-env/factory_machines_env.py
+++ factory-machines-env/factory_machines_env.py
@@ -1,30 +1,210 @@
+import math
 from typing import Optional, Union, List, Tuple
 
 import gym
 from gym import spaces
 import pygame
+import numpy as np
 from gym.core import RenderFrame, ActType, ObsType
 
 
 class FactoryMachinesEnv(gym.Env):
-
     metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
 
-    def __init__(self, render_mode=None, size=5) -> None:
+    def __init__(self, render_mode: Optional[str] = None, size=5, recipe_complexity=3) -> None:
         self.size = size  # The size of the square grid
         self.window_size = 512  # The size of the PyGame window
+        self.recipe_complexity = recipe_complexity
+
+        self.res_out = 0
+        self.res_steel = 1
+        self.res_wood = 2
+
+        self._piles = np.zeros((3, 2))
+        self._piles[self.res_out] = [0, 0]
+        self._piles[self.res_steel] = [0, size - 1]
+        self._piles[self.res_wood] = [size - 1, 0]
+
+        self._pile_colours = [(0, 0, 0)] * 3
+        self._pile_colours[self.res_out] = (80, 130, 250)
+        self._pile_colours[self.res_wood] = (70, 50, 0)
+        self._pile_colours[self.res_steel] = (168, 168, 168)
+
+        self._required = {
+            self.res_steel: 0,
+            self.res_wood: 0
+        }
+
+        self._carrying = 0  # O - nothing, 1 - steel, 2 - wood.
+
+        self._agent = np.array([])
 
         self.observation_space = spaces.Dict(
             {
-                "depot": spaces.Box(0, size - 1, shape=(2,), dtype=int),
-                "steel": spaces.Box(0, size - 1, shape=(2,), dtype=int),
-                "wood": spaces.Box(0, size - 1, shape=(2,), dtype=int),
+                "out_pile": spaces.Box(0, size - 1, shape=(2,), dtype=int),
+                "steel_pile": spaces.Box(0, size - 1, shape=(2,), dtype=int),
+                "wood_pile": spaces.Box(0, size - 1, shape=(2,), dtype=int),
+                "req_wood": spaces.Box(0, recipe_complexity, dtype=int),
+                "req_steel": spaces.Box(0, recipe_complexity, dtype=int),
                 "agent": spaces.Box(0, size - 1, shape=(2,), dtype=int),
+                "carrying": spaces.Box(0, 2, dtype=int)
             }
         )
 
+        self.action_space = spaces.Discrete(6)  # Up, down, left, right, grab, drop.
+
+        # Utility vectors for moving the agent.
+        self._action_to_direction = {
+            0: np.array([1, 0]),
+            1: np.array([0, 1]),
+            2: np.array([-1, 0]),
+            3: np.array([0, -1]),
+        }
+
+        assert render_mode is None or render_mode in self.metadata["render_modes"]
+        self.render_mode = render_mode
+
+        # Used for human friendly rendering.
+        self.window = None
+        self.clock = None
+
+    def _get_obs(self):
+        return {
+            "agent": self._agent,
+            "carrying": self._carrying,
+            "out_pile": self._piles[self.res_out],
+            "steel_pile": self._piles[self.res_steel],
+            "wood_pile": self._piles[self.res_wood],
+            "req_steel": self._required[self.res_steel],
+            "req_wood": self._required[self.res_wood],
+        }
+
+    def reset(self, *, seed: Optional[int] = None, options: Optional[dict] = None) -> Tuple[ObsType, dict]:
+        super().reset(seed=seed, options=options)
+
+        self._agent = self.np_random.integers(0, self.size)
+
+        self._required[self.res_wood] = self.np_random.integers(1, self.recipe_complexity, endpoint=True)
+        self._required[self.res_steel] = self.np_random.integers(0, self.recipe_complexity, endpoint=True)
+
+        self._carrying = 0
+
+        obs = self._get_obs()
+
+        if self.render_mode is "human":
+            self._render_human()
+
+        return obs, {}
+
     def step(self, action: ActType) -> Tuple[ObsType, float, bool, bool, dict]:
-        pass
+        illegal_move_punishment = 0
+        if action < 4:
+            # Action is a move op.
+            direction = self._action_to_direction[action]
+            self._agent = np.clip(self._agent + direction, 0, self.size - 1)
+        elif action == 4:
+            # Action is a grab op.
+            if self._try_grab() is False:
+                illegal_move_punishment = 5
+        elif action == 5:
+            # Action is a drop op.
+            if self._try_drop() is False:
+                illegal_move_punishment = 5
+
+        terminated = sum(self._required.values()) is 0
+
+        reward = 10 if terminated else 0
+        reward += illegal_move_punishment
+
+        obs = self._get_obs()
+        info = {}
+
+        return obs, reward, terminated, False, info
 
     def render(self) -> Optional[Union[RenderFrame, List[RenderFrame]]]:
+        if self.render_mode is "human":
+            return self._render_human()
+        else:
+            return self._render_ansi()
+
+    def _render_human(self):
+        assert self.render_mode is "human"
+
+        if self.window is None:
+            pygame.init()
+            pygame.display.set_caption("Factory Machines")
+            pygame.display.init()
+            self.window = pygame.display.set_mode((self.window_size, self.window_size))
+        if self.clock is None:
+            self.clock = pygame.time.Clock()
+
+        canvas = pygame.Surface((self.window_size, self.window_size))
+        canvas.fill((255, 255, 255))
+        pix_square_size = (
+                self.window_size / self.size
+        )  # The size of a single grid square in pixels
+
+        # Draw resource piles.
+        resources = [self.res_out, self.res_steel, self.res_wood]
+        for res in resources:
+            pygame.draw.rect(
+                canvas,
+                self._pile_colours[res],
+                pygame.Rect(
+                    pix_square_size * self._piles[res],
+                    (pix_square_size, pix_square_size),
+                ),
+            )
+
+        # Draw agent.
+        pygame.draw.circle(
+            canvas,
+            (0, 0, 255),
+            (self._agent + 0.5) * pix_square_size,
+            pix_square_size / 3,
+        )
+
+        # Add gridlines
+        for x in range(self.size + 1):
+            pygame.draw.line(
+                canvas,
+                0,
+                (0, pix_square_size * x),
+                (self.window_size, pix_square_size * x),
+                width=3,
+            )
+            pygame.draw.line(
+                canvas,
+                0,
+                (pix_square_size * x, 0),
+                (pix_square_size * x, self.window_size),
+                width=3,
+            )
+
+        self.window.blit(canvas, canvas.get_rect())
+        pygame.event.pump()
+        pygame.display.update()
+
+        # We need to ensure that human-rendering occurs at the predefined framerate.
+        # The following line will automatically add a delay to keep the framerate stable.
+        self.clock.tick(self.metadata["render_fps"])
+
+    def _render_ansi(self):
         pass
+
+    def _try_grab(self) -> bool:
+        if self._carrying is not 0:
+            return False
+
+        valid_res = [self.res_wood, self.res_steel]
+        for res in valid_res:
+            if np.equal(self._agent, self._piles[res]):
+                # Successfully grabbed a resource.
+                self._carrying = res
+                return True
+
+        return False
+
+    def _try_drop(self) -> bool:
+        return self._carrying is not 0 and \
+            np.equal(self._agent, self._piles[self.res_out])
