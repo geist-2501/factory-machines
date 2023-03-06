7273b2ef604c0134e2f01e6a9116696110e17704
geist-2501 <blaironbute@gmail.com>
2023-03-06 13:17:52 +0000

    Implemented recording

--- factory_machines_env/envs/fm_env_single.py
+++ factory_machines_env/envs/fm_env_single.py
@@ -7,13 +7,12 @@ from factory_machines_env.envs.fm_env_base import FactoryMachinesEnvBase
 
 class FactoryMachinesEnvSingle(FactoryMachinesEnvBase):
 
-    def __init__(self, render_mode: Optional[str] = None, map_id="0") -> None:
+    def __init__(self, render_mode: Optional[str] = None, map_id="0", order_override: str = None) -> None:
         super().__init__(render_mode, map_id)
 
-        order = np.zeros(self._num_depots)
-        while sum(order) == 0:
-            order = (np.random.normal(size=self._num_depots) > 0.5).astype(int)
-        self._depot_queues = order
+        self.order_override = order_override
+
+        self._depot_queues = self._create_order()
 
     def step(self, action: ActType) -> Tuple[ObsType, float, bool, bool, dict]:
         obs, reward, _, _, info = super().step(action)
@@ -25,10 +24,18 @@ class FactoryMachinesEnvSingle(FactoryMachinesEnvBase):
 
     def reset(self, *, seed: Optional[int] = None, options: Optional[dict] = None) -> Tuple[ObsType, dict]:
         obs, _ = super().reset(seed=seed, options=options)
-        order = np.zeros(self._num_depots)
-        while sum(order) == 0:
-            order = (np.random.normal(size=self._num_depots) > 0.5).astype(int)
-        self._depot_queues = order
+
+        self._depot_queues = self._create_order()
 
         return obs, {}
 
+    def _create_order(self):
+        if self.order_override:
+            order = np.fromstring(self.order_override, sep=',', dtype=int)
+        else:
+            # Produce a random order.
+            order = np.zeros(self._num_depots)
+            while sum(order) == 0:
+                order = (np.random.normal(size=self._num_depots) > 0.5).astype(int)
+
+        return order
