34460b7b94af4e177773a44537e1e56dbf933121
geist-2501 <blaironbute@gmail.com>
2023-03-15 10:19:58 +0000

    Added age

--- factory_machines_env/envs/fm_env_multi.py
+++ factory_machines_env/envs/fm_env_multi.py
@@ -1,4 +1,4 @@
-from typing import Optional, Tuple
+from typing import Optional, Tuple, List, Dict
 
 import numpy as np
 from gym.core import ActType, ObsType
@@ -9,20 +9,27 @@ from factory_machines_env.envs.pygame_utils import draw_lines
 
 class FactoryMachinesEnvMulti(FactoryMachinesEnvBase):
 
-    def __init__(self, render_mode: Optional[str] = None, map_id="0", num_orders=1) -> None:
+    def __init__(
+            self,
+            render_mode: Optional[str] = None,
+            map_id="0",
+            num_orders=10,
+            order_override: Dict = None,
+            timestep_override: int = None
+    ) -> None:
         super().__init__(render_mode, map_id)
 
         self._total_num_orders = num_orders
         self._num_orders_pending = num_orders
-        self._open_orders = {}
-        self._completed_orders = {}
+        self._open_orders: Dict[int, Tuple[int, np.ndarray]] = {} if order_override is None else order_override
+        self._timestep = 0 if timestep_override is None else timestep_override
 
     def reset(self, *, seed: Optional[int] = None, options: Optional[dict] = None) -> Tuple[ObsType, dict]:
         obs, _ = super().reset(seed=seed, options=options)
 
         self._num_orders_pending = self._total_num_orders
         self._open_orders = {}
-        self._completed_orders = {}
+        self._timestep = 0
 
         return obs, {}
 
@@ -37,10 +44,12 @@ class FactoryMachinesEnvMulti(FactoryMachinesEnvBase):
             order = np.zeros(self._num_depots, dtype=int)
             while sum(order) == 0:
                 order = (np.random.normal(size=self._num_depots) > 0.5).astype(int)
-            self._open_orders[order_id] = order
+            self._open_orders[order_id] = (self._timestep, order)
 
-        terminated = len(self._completed_orders) == self._total_num_orders
+        terminated = self._num_orders_pending == 0 and len(self._open_orders) == 0
         reward += 100 if terminated else 0
+
+        self._timestep += 1
 
         return obs, reward, terminated, False, info
 
@@ -48,13 +57,15 @@ class FactoryMachinesEnvMulti(FactoryMachinesEnvBase):
 
         # Draw table header.
         table_rows = [
-            "   | " + ' '.join(["D" + str(x+1) for x in range(self._num_depots)]),
-            "INV| " + self._add_table_padding(self._agent_inv),
-            "DEP| " + self._add_table_padding(self._get_depot_queues())
+            "   | T  " + ' '.join(["D" + str(x+1) for x in range(self._num_depots)]),
+            "INV|    " + self._add_table_padding(self._agent_inv),
+            "DEP|    " + self._add_table_padding(self._get_depot_queues()),
+            "AGE|    " + self._add_table_padding(self._get_depot_ages()),
         ]
 
         for order_id, order in self._open_orders.items():
-            table_rows.append(f"O{order_id:02}| " + self._add_table_padding(order))
+            order_t, order_items = order
+            table_rows.append(f"O{order_id:02}| {order_t:>2} " + self._add_table_padding(order_items))
 
         draw_lines(table_rows, self.screen, header_origin, font, self.colors["text"])
 
@@ -67,23 +78,42 @@ class FactoryMachinesEnvMulti(FactoryMachinesEnvBase):
         # Then, if an order has been completed, move it to the completed pile.
         reward = 0
         for order_id, order in self._open_orders.copy().items():
-            reward += sum(order * self._agent_inv)
+            order_t, order_items = order
+            reward += sum(order_items * self._agent_inv)
             agent_inv_inverse = 1 - self._agent_inv
-            new_order = order * agent_inv_inverse  # Clears pending items in an order.
+            new_order = order_items * agent_inv_inverse  # Clears pending items in an order.
             if sum(new_order) == 0:
                 # Order is complete!
                 reward += 10
                 del self._open_orders[order_id]
             else:
-                self._open_orders[order_id] = new_order
+                self._open_orders[order_id] = (order_t, new_order)
 
         self._agent_inv = np.zeros(self._num_depots, dtype=int)
 
         return reward
 
+    def _get_obs(self):
+        return {
+            **super()._get_obs(),
+            "depot_ages": self._get_depot_ages()
+        }
+
     def _get_depot_queues(self):
         depot_queues = np.zeros(self._num_depots, dtype=int)
         for _, order in self._open_orders.items():
-            depot_queues += order
+            _, order_items = order
+            depot_queues += order_items
 
         return depot_queues
+
+    def _get_depot_ages(self):
+        """Get the age of the oldest open order on the depots."""
+        depot_ages = np.zeros(self._num_depots, dtype=int)
+        for _, order in self._open_orders.items():
+            order_t, order_items = order
+            order_age = self._timestep - order_t
+            mask = order_items * order_age
+            depot_ages = np.maximum(depot_ages, mask)
+
+        return depot_ages
---
+++ factory_machines_env/envs/fm_env_multi_test.py
@@ -0,0 +1,15 @@
+import unittest
+
+import numpy as np
+
+from factory_machines_env.envs import FactoryMachinesEnvMulti
+
+
+class FMFlattenRelativeTest(unittest.TestCase):
+    def should_terminate(self):
+        # TODO
+        pass
+
+    def should_get_correct_depot_age(self):
+        # TODO
+        pass
