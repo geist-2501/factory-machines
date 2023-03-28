08659a4f9874bc6b9db7d1a3c99fa418a451702a
geist-2501 <blaironbute@gmail.com>
2023-03-28 13:27:03 +0100

    errata

--- agents/tests/h_dqn_agent_test.py
+++ agents/tests/h_dqn_agent_test.py
@@ -1,11 +1,11 @@
 import unittest
 
-from agents.h_dqn_agent import TimeKeeper
+from agents.h_dqn_agent import KCatchUpTimeKeeper
 
 
 class HDQNAgentTest(unittest.TestCase):
     def test_timekeeper_should_allow_q1_training(self):
-        timekeeper = TimeKeeper()
+        timekeeper = KCatchUpTimeKeeper()
         self._step(timekeeper, 3)
         timekeeper.set_k_catch_up(5)
         self.assertTrue(timekeeper.should_train_q1())
@@ -17,12 +17,12 @@ class HDQNAgentTest(unittest.TestCase):
         self.assertTrue(timekeeper.should_train_q1())
 
     @staticmethod
-    def _step(timekeeper: TimeKeeper, num: int):
+    def _step(timekeeper: KCatchUpTimeKeeper, num: int):
         for _ in range(num):
             timekeeper.step()
 
     @staticmethod
-    def _meta(timekeeper: TimeKeeper, num: int):
+    def _meta(timekeeper: KCatchUpTimeKeeper, num: int):
         for _ in range(num):
             timekeeper.meta_step()
 
